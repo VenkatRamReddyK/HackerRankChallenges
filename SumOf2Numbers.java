8a03bca89db02cfa7c4dadc745a8973bd421708f
VENKAT RAM REDDY KUNTA <venkat.csulb@gmail.com>
2015-10-17 12:31:24 -0700

    Interview Preparation

---
+++ SumOf2Numbers.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package InteviewPreparation;
+
+/**
+ *
+ * @author venkatramreddykunta
+ */
+public class SumOf2Numbers {
+
+    public static void main(String[] args) {
+        int[] list={1,2,3,6,7,4,1,6,0,8,-1};
+        int sum=7; // 70,61,61,8-1,43
+        sumInList();
+    }
+
+    private static void sumInList() {
+        
+    }
+}
